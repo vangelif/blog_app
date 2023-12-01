f35f190fcc129312b27a24a483668f8f9a7c4612
vangelif <ev.fotiades@yahoo.com>
2023-12-01 12:52:28 +0200

    build rspec tests for post model class

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,52 +1,51 @@
-# Title must not be blank.
-# Title must not exceed 250 characters.
-# CommentsCounter must be an integer greater than or equal to zero.
-# LikesCounter must be an integer greater than or equal to zero.
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe 'validations' do
+describe Post, type: :model do
+  before do
+    @user = User.create(name: 'Jonathan',
+                   photo: 'https://unsplash.com/photos/man-in-white-crew-neck-t-shirt-kissing-woman-in-white-dress-Z39a7lqZusU', bio: 'I love my job', posts_counter: 0)
+  end
+  
+  subject { Post.new(author: @user, title: 'This is a post title!', text: 'My first post is about rails framework!') }
+
+  before { subject.save }
+
+  describe 'Post validations' do
+    # Title must not be blank.
     it 'title must not be blank' do
       post = Post.new(title: nil)
       expect(post).to_not be_valid
     end
-
+    # Title must not exceed 250 characters.
     it 'title must not exceed 250 characters' do
       post = Post.new(title: 'a' * 251)
-      post.valid?
-      expect(post.errors[:title]).to include("it's too long! -- maximum 250 characters")
-
-      post.title = 'a' * 250
-      post.valid?
-      expect(post.errors[:title]).to be_empty
+      expect(post).to be_invalid
+      expect(post.errors[:title]).to include('is too long (maximum is 250 characters)')
+    end
+    # CommentsCounter must be an integer.
+    it 'comments_counter must be an integer' do
+      subject.comments_counter = 'apples'
+      expect(subject).to_not be_valid
     end
 
-    it 'CommentsCounter must be an integer and greater than or equal to zero' do
-      post = Post.new(comments_counter: 'not_an_integer')
-      post.valid?
-      expect(post.errors[:comments_counter]).to include('must be an integer!')
-
-      post.comments_counter = -1
-      post.valid?
-      expect(post.errors[:comments_counter]).to include('must be greater or equal to 0')
-
-      post.comments_counter = 5
-      post.valid?
-      expect(post.errors[:comments_counter]).to be_empty
+    # CommentsCounter must be greater than or equal to zero.
+    it 'comments_counter and likes_counter should be equal to zero' do
+      subject.comments_counter = 0
+      subject.likes_counter = 0
+      expect(subject).to be_valid
+    end
+    # LikesCounter must be an integer.
+    it 'likes_counter must be an integer' do
+      subject.likes_counter = 'bananas'
+      expect(subject).to_not be_valid
     end
 
-    it 'LikesCounter must be an integer and greater than or equal to zero' do
-      post = Post.new(likes_counter: 'not_an_integer')
-      post.valid?
-      expect(post.errors[:likes_counter]).to include('must be an integer!')
-
-      post.likes_counter = -1
-      post.valid?
-      expect(post.errors[:likes_counter]).to include('must be greater than or equal to 0')
-
-      post.likes_counter = 5
-      post.valid?
-      expect(post.errors[:likes_counter]).to be_empty
+    # LikesCounter must be greater than or equal to zero.
+    it 'comments_counter and likes_counter should be greater than zero' do
+      subject.comments_counter = -5
+      expect(subject).to_not be_valid
+      subject.likes_counter = -5
+      expect(subject).to_not be_valid
     end
   end
 end
