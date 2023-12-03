90266ed91a14e947b6574ada6fab95cb54de19da
vangelif <ev.fotiades@yahoo.com>
2023-12-03 19:38:10 +0200

    add comments for easy db reference

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,20 @@
+# Table "public.comments"
+# Column   |              Type              | Collation | Nullable |               Default
+# ------------+--------------------------------+-----------+----------+--------------------------------------
+# id         | bigint                         |           | not null | nextval('comments_id_seq'::regclass) 
+# text       | text                           |           |          |
+# user_id    | bigint                         |           | not null |
+# post_id    | bigint                         |           | not null |
+# created_at | timestamp(6) without time zone |           | not null |
+# updated_at | timestamp(6) without time zone |           | not null |
+# Indexes:
+#  "comments_pkey" PRIMARY KEY, btree (id)
+#  "index_comments_on_post_id" btree (post_id)
+#  "index_comments_on_user_id" btree (user_id)
+# Foreign-key constraints:
+#  "fk_rails_03de2dc08c" FOREIGN KEY (user_id) REFERENCES users(id)
+#  "fk_rails_2fd19c0db7" FOREIGN KEY (post_id) REFERENCES posts(id)
+
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,31 @@
+# Table "public.posts"
+# Column      |              Type              | Collation | Nullable |              Default
+# ------------------+--------------------------------+-----------+----------+-----------------------------------
+# id               | bigint                         |           | not null | nextval('posts_id_seq'::regclass)
+# title            | character varying              |           |          |
+# text             | text                           |           |          |
+# comments_counter | integer                        |           |          | 0
+# likes_counter    | integer                        |           |          | 0
+# author_id        | bigint                         |           | not null |
+# created_at       | timestamp(6) without time zone |           | not null |
+# updated_at       | timestamp(6) without time zone |           | not null |
+# Indexes:
+# "posts_pkey" PRIMARY KEY, btree (id)
+# "index_posts_on_author_id" btree (author_id)
+# Foreign-key constraints:
+# "fk_rails_5b5ddfd518" FOREIGN KEY (author_id) REFERENCES users(id)
+# Referenced by:
+# TABLE "comments" CONSTRAINT "fk_rails_2fd19c0db7" FOREIGN KEY (post_id) REFERENCES posts(id)
+# TABLE "likes" CONSTRAINT "fk_rails_87a8aac469" FOREIGN KEY (post_id) REFERENCES posts(id)
+
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  # these are callbacks that will be executed 
+  # by calling the private methods 
+  # when a new post is created or destroyed
   after_create :increment_users_counter
   after_destroy :decrement_users_counter
 
@@ -17,10 +40,16 @@ class Post < ApplicationRecord
   private
 
   def increment_users_counter
+    # increment! is an ActiveRecord method
+    # for increasing the value of an attribute by 1
+    # :posts_counter is an attribute on User's table
     author.increment!(:posts_counter)
   end
 
   def decrement_users_counter
+        # decrement! is an ActiveRecord method
+    # for decreasing the value of an attribute by 1
+    # :posts_counter is an attribute on User's table
     author.decrement!(:posts_counter)
   end
 end
