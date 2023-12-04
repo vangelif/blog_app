2493ff62c37a314debbb3da296f9dcec870db4fb
vangelif <ev.fotiades@yahoo.com>
2023-12-04 14:21:34 +0200

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@
 #     TABLE "likes" CONSTRAINT "fk_rails_1e09b5dabf" FOREIGN KEY (user_id) REFERENCES users(id)
 #     TABLE "posts" CONSTRAINT "fk_rails_5b5ddfd518" FOREIGN KEY (author_id) REFERENCES users(id)
 
-
 class User < ApplicationRecord
   # the user has many posts and the the foreign key in the posts is author_id
   # when the user is deleted the dependent: :destroy option ensures that all posts
