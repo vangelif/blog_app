4f11f93a2fb80e163011ada391e1e3ca0acd30c5
vangelif <ev.fotiades@yahoo.com>
2023-12-05 14:20:42 +0200

    build request specs for posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:id/posts' do
+    it 'returns http success' do
+      get '/users/2/posts'
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns success response 200' do
+      get '/users/2/posts'
+      expect(response.status).to eq(200)
+    end
+    it 'returns body within the page' do
+      get '/users/2/posts'
+      expect(response.body).to include('Find me in')
+    end
+  end
+
+  describe 'GET /users/:id/posts/:id' do
+    it 'returns http success' do
+      get '/users/2/posts/1'
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns success response 200' do
+      get '/users/2/posts/1'
+      expect(response.status).to eq(200)
+    end
+    it 'returns body within the page' do
+      get '/users/2/posts/1'
+      expect(response.body).to include('Posts#show')
+    end
+  end
+end
