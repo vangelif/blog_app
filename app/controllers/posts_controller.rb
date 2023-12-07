3f256997ddd440366d29653128482342354c8e61
vangelif <ev.fotiades@yahoo.com>
2023-12-07 15:35:03 +0200

    refactor model to fix case of no comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.includes(:author).includes(:comments).where(author: params[:user_id]).order(created_at: :asc)
+    # when building the show all posts button
+    @author = @posts.first.author
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 
+
+  def has_posts?
+    three_most_recents_posts.present?
+  end
+  
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
