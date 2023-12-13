86556aa1b1463a5314bbeb2a03b541213a9500ff
vangelif <ev.fotiades@yahoo.com>
2023-12-13 16:56:10 +0200

    build application controller for authenticating user before any other action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
-  end
+  # no matter which route we log in, we will be redirected 
+  # to the sign in page or sign up page first
+  before_action :authenticate_user!
 end
